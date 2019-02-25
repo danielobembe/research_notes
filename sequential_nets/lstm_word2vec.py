a37d67a56349ed3b9ba03672779169a0a681cc66
Daniel Obembe <obembe.dan@gmail.com>
2019-02-24 19:03:46 -0600

    Loading and using a word-embedding

---
+++ sequential_nets/lstm_word2vec.py
@@ -0,0 +1,5 @@
+"""
+LSTM tutorial  - using LSTM with pre-trained word2vec.
+source: http://www.brightideasinanalytics.com/pretrained-word-vectors-example/
+source: http://www.brightideasinanalytics.com/rnn-pretrained-word-vectors/
+"""
---
+++ sequential_nets/lstm_word2vec_2.py
@@ -0,0 +1,43 @@
+"""
+LSTM tutorial  - using LSTM with pre-trained word2vec.
+source: http://www.brightideasinanalytics.com/pretrained-word-vectors-example/
+source: http://www.brightideasinanalytics.com/rnn-pretrained-word-vectors/
+"""
+
+#Note: word embeddings === word vectors
+filename = 'glove.6B.50d.txt'
+
+
+glove_vocab     = []    #list of words we have embeddings for
+glove_embed     = []    #list of lists containing embedding vectors 
+embedding_dict  = {}    #dict{ word : embedding }
+
+file = open(filename, 'r', encoding='UTF-8')
+
+for line in file.readlines():
+    row = line.strip().split(' ')
+    vocab_word = row[0]
+    glove_vocab.append(vocab_word)
+    embed_vector = [float(i) for i in row[1:]]
+    embedding_dict[vocab_word] = embed_vector
+    glove_embed.append(embed_vector)
+
+print('Loaded GLOVE\n')
+file.close()
+
+# print("man: ", embedding_dict['man'])
+
+from scipy import spatial
+import numpy as np 
+
+king_vector = np.array(embedding_dict['king'])
+man_vector  = np.array(embedding_dict['man'])
+woman_vector = np.array(embedding_dict['woman'])
+
+new_vector = king_vector - man_vector + woman_vector
+
+tree = spatial.KDTree(glove_embed)
+
+nearest_dist, nearest_idx = tree.query(new_vector, 10)
+nearest_words = [glove_vocab[i] for i in nearest_idx]
+print("Nearest words to 'king - man + woman: ", nearest_words)
